c889cfb351b10be65a9ff6331c87ffb4b374f5a1
RobertRuai <robertruai4@gmail.com>
2022-12-09 07:32:18 -0800

    task 6 check

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print 0-9 using putchar
+ *
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
