371c351e4078e0cc804a02d7c29ba0da572f903d
RobertRuai <robertruai4@gmail.com>
2023-01-17 13:34:09 -0800

    a function that creates a new dog, stores a copy of name and owner

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,54 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog - entry point
+ *
+ * description - a function that creates a new dog.
+ *
+ * @name: The character to print
+ * @age: The character to print
+ * @owner: The character to print
+ *
+ * Return: Always 0 (Success)
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, l = 0, k = 0;
+	dog_t *d;
+
+	while (name[l] != '\0')
+		l++;
+	while (owner[k] != '\0')
+		k++;
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	d->name = malloc(l * sizeof(d->name));
+	if  (d->name == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
+	for (i = 0; i <= l; i++)
+		d->name[i] = name[i];
+	d->age = age;
+
+	d->owner = malloc(k * sizeof(d->owner));
+	if (d->owner == NULL)
+	{
+		free(d->owner);
+		free(d);
+		return (NULL);
+	}
+	for (i = 0; i <= k; i++)
+		d->owner[i] = owner[i];
+
+	return (d);
+}
