84a86d81c1fa4f0ab821489c5b22dd69a6f789b2
RobertRuai <robertruai4@gmail.com>
2023-01-19 06:04:46 -0800

    a function that executes a function given as a parameter on each element of an array.

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ *  - entry point
+ *
+ * description -
+ *
+ * @c: The character to print
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
