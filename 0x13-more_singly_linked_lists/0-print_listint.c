52c9ad88cb6c1fd622c9d3f56c85ddb73a965dd6
RobertRuai <robertruai4@gmail.com>
2023-02-01 08:24:55 -0800

    minor change

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,11 +15,12 @@ size_t print_listint(const listint_t *h)
 	if (h == NULL)
 		return (0);
 
-	while (h != NULL)
+	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
+	printf("%d\n", h->n);
 	return (count);
 }
